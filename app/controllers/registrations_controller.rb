e6986dba5c15f97d3d8f621e7d55c571fc6f8be2
charly philippe <charly.ph@hotmail.fr>
2023-01-12 15:56:11 +0100

    corrections in registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,16 +2,18 @@ class RegistrationsController < ApplicationController
   def create
     user = User.create!(
       email: params['user']['email'],
-      password:  params['user']['email'],
-      password_confirmation: params['user']['password_confirmation'],
+      password: params['user']['password'],
+      password_confirmation: params['user']['password_confirmation']
     )
 
-  if user
-    session[:user_id] = user.id render json: {
-      status: :created,
-      user: user
-    }
-  else
-    render :new, status: :unprocessable_entity
+    if user
+      session[:user_id] = user.id
+      render json: {
+        status: :created,
+        user: user
+      }
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
