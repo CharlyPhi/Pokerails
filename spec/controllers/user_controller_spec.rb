b947204052d05319baed2052894e3f93361e0ac8
charly philippe <charly.ph@hotmail.fr>
2023-03-22 21:10:21 +0100

    first step

---
+++ spec/controllers/user_controller_spec.rb
@@ -0,0 +1,9 @@
+require "rails-helper"
+
+RSpec.describe UsersController, type: :controller do
+  describe 'User #create' do
+    it 'can create a user' do
+      user = user.create
+    end
+end
+end
